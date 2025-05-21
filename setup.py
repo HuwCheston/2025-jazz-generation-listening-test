df0a8f5f9f492849ce996d8ec1cb5ea0e5ceaa4b
Huw Cheston <hwc31@cam.ac.uk>
2025-05-21 11:55:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jazz_generation_listening_test',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Listening test for symbolic jazz generation model',
+    author='Huw Cheston',
+    license='MIT',
+)
